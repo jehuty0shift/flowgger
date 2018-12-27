3c0ac9b98389e9ba2b5e70b77b7e421652f8c0c5
Babacar Diassé <babacar.diasse@corp.ovh.com>
2018-12-27 16:08:25 +0100

    re-add threading support back in Kafka Output

--- src/flowgger/output/kafka_output.rs
+++ src/flowgger/output/kafka_output.rs
@@ -10,8 +10,11 @@ use std::collections::BTreeMap;
 use toml::Value;
 
 pub struct KafkaOutput {
-    config: KafkaConfig
+    config: KafkaConfig,
+    threads: u32,
 }
+
+const KAFKA_DEFAULT_THREADS: u32 = 1;
 
 #[derive(Clone)]
 struct KafkaConfig {
@@ -21,8 +24,8 @@ struct KafkaConfig {
 
 struct KafkaWorker {
     arx: Arc<Mutex<Receiver<Vec<u8>>>>,
-    producer: FutureProducer,
     config: KafkaConfig,
+    producer: FutureProducer,
 }
 
 impl KafkaWorker {
@@ -63,11 +66,18 @@ impl KafkaOutput {
             .lookup("output.librdkafka").expect("output.librdkafka must be set")
             .as_table().expect("output.librdkafka must be set")
             .to_owned();
+        let threads = config
+            .lookup("output.kafka_threads")
+            .map_or(KAFKA_DEFAULT_THREADS, |x| {
+                x.as_integer()
+                    .expect("output.kafka_threads must be a 32-bit integer") as u32
+            });
         KafkaOutput {
             config: KafkaConfig {
                 topic: topic,
                 librdkafka: librdconfig,
-            }
+            },
+            threads: threads
         }
     }
 }
@@ -77,11 +87,22 @@ impl Output for KafkaOutput {
         if merger.is_some() {
             error!("Output framing is ignored with the Kafka output");
         }
+<<<<<<< HEAD
         let tarx = Arc::clone(&arx);
         let tconfig = self.config.clone();
         thread::spawn(move || {
             let mut worker = KafkaWorker::new(tarx, tconfig);
             worker.run();
         });
+=======
+        for id in 0..self.threads {
+            let tarx = Arc::clone(&arx);
+            let tconfig = self.config.clone();
+            thread::Builder::new().name(format!("kafka-output-{}",id)).spawn(move || {
+                let mut worker = KafkaWorker::new(tarx, tconfig);
+                worker.run();
+            }).unwrap();
+        }
+>>>>>>> dd9905c... re-add threading support back in Kafka Output
     }
 }
